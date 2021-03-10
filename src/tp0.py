c43759617b3da18c825a5b1fe481d782e52d8fc9
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-03-10 10:10:20 -0300

    Added std and quantiles

--- src/tp0.py
+++ src/tp0.py
@@ -1,12 +1,11 @@
 import pandas as pd
-import math
 from sklearn.model_selection import train_test_split
 
 # import file
-df = pd.read_csv('res/tp0/marambio_2007.dat', sep=r'\s+')
+df = pd.read_csv('../res/tp0/marambio_2007.dat', sep=r'\s+')
 
 # create new data frame
-stats = pd.DataFrame(index=['Mean', 'Max', 'Min', 'Median'], columns=df.columns)
+stats = pd.DataFrame(index=['Mean', 'Max', 'Min', 'Median', 'Std', '25%', '50%', '75%'], columns=df.columns)
 
 # fill new df for each model
 for col in df:
@@ -14,11 +13,17 @@ for col in df:
     stats[col][1] = df[col].max()
     stats[col][2] = df[col].min()
     stats[col][3] = df[col].median()
+    stats[col][4] = df[col].std()
+    stats[col][5] = df[col].quantile(.25)
+    stats[col][6] = df[col].quantile(.75)
+    stats[col][7] = df[col].quantile(.5)
 
 print(stats)
+print("\n\n")
 
 # Split dataframes in test and train dataframes
 train, test = train_test_split(df, test_size=0.2)
 
+print("\n\n")
 print(train.shape[0])
 print(test.shape[0])
