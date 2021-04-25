ac13bbdaffe8126958b152711ad3b01db9f10228
mlund <mlund@itba.edu.ar>
2021-04-25 20:44:44 -0300

    Add ID3 classify (WIP)

--- src/tp2/tree.py
+++ src/tp2/tree.py
@@ -1,8 +1,13 @@
+from enum import Enum
+
+NodeType = Enum('NodeType', 'attr val leaf')
+
 class Tree:
     class Node:
-        def __init__(self, value=None):
+        def __init__(self, node_type, value=None):
             self.children = []
             self.value = value
+            self.node_type = node_type
         
         def __str__(self):
             curr = f'\n{self.value}\n'
@@ -10,13 +15,13 @@ class Tree:
                 curr += str(child)
             return curr
         
-        def add_child(self, child_value):
-            child = Tree.Node(child_value)
+        def add_child(self, node_type, child_value):
+            child = Tree.Node(node_type, child_value)
             self.children.append(child)
             return child
     
     def __init__(self, root_value=None):
-        self.root = Tree.Node(root_value)
+        self.root = Tree.Node(NodeType.attr, root_value)
     
     def __str__(self):
         return str(self.root)
